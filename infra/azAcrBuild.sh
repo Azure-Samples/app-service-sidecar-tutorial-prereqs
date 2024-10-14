35d14fd5d836e471db46fa0751c5031c2be1482f
Cephas Lin <cephalin@users.noreply.github.com>
2024-10-14 15:39:16 +0200

    Update azAcrBuild.sh

--- infra/azAcrBuild.sh
+++ infra/azAcrBuild.sh
@@ -7,7 +7,7 @@ az account set -s $AZURE_SUBSCRIPTION_ID
 printf "\nBuilding and uploading nginx image. Please wait..."
 az acr build --resource-group $GROUP_NAME \
   --registry $ACR_NAME \
-  --image nginx https://github.com/lcephas/template.git#main:images/nginx \
+  --image nginx https://github.com/Azure-Samples/app-service-sidecar-tutorial-prereqs.git#main:images/nginx \
   --file Dockerfile \
   --platform linux \
   --no-logs \
@@ -17,7 +17,7 @@ printf "Success!"
 printf "Building and uploading otel-collector image. Please wait..."
 az acr build --resource-group $GROUP_NAME \
   --registry $ACR_NAME \
-  --image otel-collector https://github.com/lcephas/template.git#main:images/otel-collector \
+  --image otel-collector https://github.com/Azure-Samples/app-service-sidecar-tutorial-prereqs.git#main:images/otel-collector \
   --file Dockerfile \
   --platform linux \
   --no-logs \
